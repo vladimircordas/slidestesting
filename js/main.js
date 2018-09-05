16332bf3d01059282c1e02155c6cba3479ac1c1c
VladTheImplier <tt_cox@hotmail.com>
2018-09-05 23:26:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,37 +1,5 @@
 $(document).ready(function() {
-	$('#pagepiling').pagepiling({
-	    menu: "#myMenu",
-        direction: 'horizontal',
-        verticalCentered: true,
-        sectionsColor: [],
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
-        scrollingSpeed: 1500,
-        easing: 'swing',
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        navigation: {
-            'textColor': '#000',
-            'bulletsColor': '#000',
-            'position': 'right',
-            'tooltips': ['section1', 'section2', 'section3', 'section4']
-        },
-       	normalScrollElements: null,
-        normalScrollElementTouchThreshold: 5,
-        touchSensitivity: 5,
-        keyboardScrolling: true,
-        sectionSelector: '.section',
-        animateAnchor: false,
 
-		//events
-		onLeave: function(index, nextIndex, direction){
-            if(direction =='left' || direction == 'right'){
-                $("video").each(function () { this.pause() });
-            }
-        },
-		afterLoad: function(anchorLink, index){},
-		afterRender: function(){},
-    });
     
     $(window).bind('mousewheel', function(event) {
         if (event.originalEvent.wheelDelta >= 0) {
